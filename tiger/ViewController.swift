3cef4ca03a2ec25d4bece68bb6e3096017219801
pari <pammygollapudi@gmail.com>
2014-11-02 17:41:52 -0500

    First commit

--- tiger/ViewController.swift
+++ tiger/ViewController.swift
@@ -9,10 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBAction func randomButtonClick(sender: AnyObject) {
+    let randomInt = Int(arc4random_uniform(UInt32(tigerArray.count)))
+        i = randomInt
+       let c = tigerArray[i]
 
+        UIView.transitionWithView(self.view, duration: 0.4, options:UIViewAnimationOptions.TransitionFlipFromRight, animations: {
+        
+            self.ageLabel.text = String(c.age)
+            self.nameLabel.text = c.name
+        
+        
+            }, completion: {(finished: Bool) -> () in })
+    }
+    var tigerArray: [TigerList] = []
+    var i:Int = 0
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBAction func nextitem(sender: UIBarButtonItem) {
+        ++i
+       if i >= tigerArray.count
+        {
+            i = 0
+        }
+        
+        ageLabel.text = String(tigerArray[i].age)
+        nameLabel.text = tigerArray[i].name
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
+        
+        tigerArray = [TigerList(age:2 , name:"BEngal") ,TigerList(age:3 , name:"Bengal1")  , TigerList(age:4 , name:"Bengal2")]
+        if i < tigerArray.count
+        {
+          ageLabel.text = String(tigerArray[i].age)
+            nameLabel.text = tigerArray[i].name
+
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
---
+++ tigerstruct.swift
@@ -0,0 +1,19 @@
+//
+//  tigerstruct.swift
+//  tiger
+//
+//  Created by Parimala Gollapudi on 11/2/14.
+//  Copyright (c) 2014 Parimala Gollapudi. All rights reserved.
+//
+
+import Foundation
+import UIkit
+
+struct TigerList
+{
+    
+    var age : Int
+    var name : String
+    
+}
+

139099cb0cb6104d0c042b7cfec4ba672ca034a2
pari <pammygollapudi@gmail.com>
2014-11-02 11:25:42 -0500

    Initial Commit

---
+++ tiger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tiger
+//
+//  Created by Parimala Gollapudi on 11/2/14.
+//  Copyright (c) 2014 Parimala Gollapudi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
